81bc9d6eee87ebf1981817a9cf434db0adceec49
Sharan S <sharansr@pm.me>
2022-12-01 13:57:29 -0800

    Added haskell solution to day 1

---
+++ 2022/1-calorie-counting.hs
@@ -0,0 +1,14 @@
+import Data.List
+import Data.List.Split
+import System.IO
+
+main = do
+  file <- readFile "data.txt"
+  let res = (reverse . sort . parseInput) file
+  print $ head res
+  print $ sum $ take 3 res
+
+parseInput :: String -> [Int]
+parseInput input = 
+  fmap (sum . map read . (splitOn "\n"))
+  (splitOn "\n\n" input)
