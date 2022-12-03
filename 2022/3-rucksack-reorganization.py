e496931bbfe030cff40f40a75ab79ecc092c442f
Sharan S <sharansr@pm.me>
2022-12-02 23:47:58 -0800

    Simplify day 3 code a bit

--- 2022/3-rucksack-reorganization.py
+++ 2022/3-rucksack-reorganization.py
@@ -1,7 +1,6 @@
 from functools import reduce
 
-ABC = 'abcdefghijklmnopqrstuvwxyz'
-ABC += ABC.upper()
+ABC = '.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def find_unique_char(*strs: list[str]) -> str:
   return list(reduce(lambda a, b: set(a) & set(b), strs))[0]
@@ -13,9 +12,9 @@ with open('data.txt', 'r') as file:
   for i, line in enumerate(file, 1):
     line = line.strip()
     comp1, comp2 = line[:len(line)//2], line[len(line)//2:]
-    part_1 += ABC.index(find_unique_char(comp1, comp2)) + 1
+    part_1 += ABC.index(find_unique_char(comp1, comp2))
     if i % 3 == 0:
-      part_2 += ABC.index(find_unique_char(*group_inventory, line)) + 1
+      part_2 += ABC.index(find_unique_char(*group_inventory, line))
       group_inventory = []
     else:
       group_inventory.append(line)
