0b4b63900dcf8a2bd1385b2c9be99f659375c585
Sharan S <sharansr@pm.me>
2022-12-02 23:39:23 -0800

    Added day 3 python sol'n

---
+++ 2022/3-rucksack-reorganization.py
@@ -0,0 +1,23 @@
+from functools import reduce
+
+ABC = 'abcdefghijklmnopqrstuvwxyz'
+ABC += ABC.upper()
+
+def find_unique_char(*strs: list[str]) -> str:
+  return list(reduce(lambda a, b: set(a) & set(b), strs))[0]
+
+part_1, part_2 = 0, 0
+group_inventory = []
+
+with open('data.txt', 'r') as file:
+  for i, line in enumerate(file, 1):
+    line = line.strip()
+    comp1, comp2 = line[:len(line)//2], line[len(line)//2:]
+    part_1 += ABC.index(find_unique_char(comp1, comp2)) + 1
+    if i % 3 == 0:
+      part_2 += ABC.index(find_unique_char(*group_inventory, line)) + 1
+      group_inventory = []
+    else:
+      group_inventory.append(line)
+
+print(part_1, part_2)
